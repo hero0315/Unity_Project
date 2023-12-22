059309279d5dd9a2d24d6140545da69c04c31879
hero0315 <38177176+hero0315@users.noreply.github.com>
2023-12-22 16:10:12 +0800

    1221#1

--- Project/Assets/script/UIScript/Timer.cs
+++ Project/Assets/script/UIScript/Timer.cs
@@ -1,41 +1,45 @@
 using TMPro;
 using UnityEngine;
-
+using System.Collections;
 public class Timer : MonoBehaviour
 {
     public static float time=0f;
     private float min=0;
     [SerializeField]private TextMeshProUGUI timeText;
-    void Start()
+    void Awake()
     {
-        InvokeRepeating("PastaSecond",1f,1f);
+        time=0;
+        StartCoroutine(Timepast());
     }
-    void PastaSecond(){
-        time+=1;
-        min=Mathf.Floor(time/60);
-        if(min==0){
-            if(time%60<10){
-                timeText.text="00" +" : 0" +time%60;
+    IEnumerator Timepast(){
+        while(true){
+            time+=1;
+            min=Mathf.Floor(time/60);
+            if(min==0){
+                if(time%60<10){
+                    timeText.text="00" +" : 0" +time%60;
+                }
+                else{
+                    timeText.text="00" +" : " +time%60;
+                }
+            }
+            else if(min<10){
+                if(time%60<10){
+                    timeText.text="0" +min+" : 0" +time%60;
+                }
+                else{
+                    timeText.text="0"+min +" : " +time%60;
+                }
             }
             else{
-                timeText.text="00" +" : " +time%60;
-            }
-        }
-        else if(min<10){
-            if(time%60<10){
-                timeText.text="0" +min+" : 0" +time%60;
-            }
-            else{
-                timeText.text="0"+min +" : " +time%60;
-            }
-        }
-        else{
-            if(time%60<10){
-                timeText.text= +min+" : 0" +time%60;
-            }
-            else{
+                if(time%60<10){
+                    timeText.text= +min+" : 0" +time%60;
+                }
+                else{
                 timeText.text=  min +" : " +time%60;
+                }
             }
+            yield return new WaitForSeconds(1f);
         }
     }
     public float gettime(){
